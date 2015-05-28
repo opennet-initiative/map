29739fde924e6b54e5fc17cade198c4e3c7627a0
matthias <mame@plattform32.de>
2015-05-28 21:36:39 +0200

    APs nun mit bbox Filter

--- server.py
+++ server.py
@@ -1,5 +1,6 @@
 from bottle import route, template, request, response, run, static_file
 from geojson import FeatureCollection, Feature, Point, LineString
+from shapely import geometry
 import geojson
 import logging
 import geronimo_api
@@ -7,26 +8,48 @@ import geronimo_api
 @route('/api/accesspoints')
 def listAccesspoints():
     apiformat = request.query.format or 'json'
+    bbox = request.query.bbox
     if apiformat == "json":
         response.content_type = 'text/json; charset=UTF8'
         features = []
-        for ap in api.getAccesspoints():
-            feature = Feature(ap.main_ip, Point((ap.lat, ap.lon)), ap.__dict__)
-            features.append(feature) 
-        collection = FeatureCollection(features)
-        return geojson.dumps(collection)
+        if bbox == "":
+            #return all APs
+            for ap in api.getAccesspoints():
+                feature = Feature(ap.main_ip, Point((ap.lat, ap.lon)), ap.__dict__)
+                features.append(feature) 
+            collection = FeatureCollection(features)
+            return geojson.dumps(collection)
+        else:
+            #only APs within the bbox
+            bbox = bbox.split(",")
+            bbox = geometry.geo.box(float(bbox[0]),float(bbox[1]),float(bbox[2]),float(bbox[3]))
+            for ap in api.getAccesspoints():
+                p = geometry.Point((ap.lat, ap.lon))
+                if bbox.contains(p):
+                    feature = Feature(ap.main_ip, p, ap.__dict__)
+                    features.append(feature) 
+            collection = FeatureCollection(features)
+            return geojson.dumps(collection)
+            
+            
 
 @route('/api/links')
 def listLinks():
     apiformat = request.query.format or 'json'
+    bbox = request.query.bbox
     if apiformat == "json":
         response.content_type = 'text/json; charset=UTF8'
         features = []
-        for link in api.getLinks():
-            geom=LineString([(link.ap1.lat, link.ap1.lon), (link.ap2.lat, link.ap2.lon)])
-            feature = Feature('link', geom, {"lq":link.lq,"rlq":link.rlq})
-            features.append(feature) 
-        collection = FeatureCollection(features)
+        if bbox is None:
+            #return all links
+            for link in api.getLinks():
+                geom=LineString([(link.ap1.lat, link.ap1.lon), (link.ap2.lat, link.ap2.lon)])
+                feature = Feature('link', geom, {"lq":link.lq,"rlq":link.rlq})
+                features.append(feature) 
+            collection = FeatureCollection(features)
+        else:
+            #only links touching the bbox
+            pass
         return geojson.dumps(collection)
 
 @route('/static/<filepath:path>')
