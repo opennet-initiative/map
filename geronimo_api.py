d3e5b4a6c084756f7758e82208d73555a24ab597
matthias <mame@plattform32.de>
2016-01-23 22:45:33 +0100

    add unicode casts

--- geronimo_api.py
+++ geronimo_api.py
@@ -153,8 +153,8 @@ class Api(threading.Thread):
         logging.info("found %d accesspoint sites" % (len(self.sites)))
         #detect cables between sites
         #TODO: allg. sites ausgeben und als primitiver Datentyp
-        university=["Ulmenstraße 69","August-Bebel-Straße 28","Albert Einsteinstraße 22"]
-        government=["HWBR","Holbeinplatz 14, Bauamt", "Industriestraße 12"]
+        university=[u"Ulmenstraße 69",u"August-Bebel-Straße 28",u"Albert Einsteinstraße 22"]
+        government=[u"HWBR",u"Holbeinplatz 14, Bauamt", u"Industriestraße 12"]
         cablesites=[]
         for site in self.sites:
             if site.name in university:
